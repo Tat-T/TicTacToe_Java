d4d2d87f1264aed1d5573e6e29c12cf6cf4adf04
Tat-T <ring_87_87@mail.ru>
2025-03-19 20:32:37 +0300

    git

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -25,6 +25,43 @@ public class TicTacToe {
         return false;
     }
 
+//    public boolean checkWin(){
+//
+//    }
+
+
+
+
+
+
+    public boolean checkWin() {
+        // строки
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+
+        // столбцы
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] != '-' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+                return true;
+            }
+        }
+
+        // главную диагональ
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+
+        // Проверяем побочную диагональ
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;
+        }
+
+        return false;
+    }
+
     public String getBoard() {
         StringBuilder out = new StringBuilder();
         for (char[] row: board){
